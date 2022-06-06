8ad58ae8db5ac841c6a3db1019f9ab14b53b32df
brie <briememo95@gmail.com>
2022-06-06 21:02:06 +0300

    add routing

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,3 +1,192 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+from django.views.generic import RedirectView
+from cloudinary.forms import cl_init_js_callbacks
 
-# Create your views here.
+from z_gram.forms import RegisterForm, UserPostForm, UpdateUserForm, UpdateUserProfileForm, CommentForm
+from z_gram.models import UserPost, Follow, UserProfile
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'django_registration/registration_form.html', {'form': form})
+
+
+@login_required(login_url='login')
+def home(request):
+    """
+    main view that handles rendering home page
+    """
+    all_images = UserPost.objects.all()
+    all_users = User.objects.exclude(id=request.user.id)
+    if request.method == 'POST':
+        form = UserPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.userprofile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = UserPostForm()
+    params = {
+        'images': all_images,
+        'users': all_users,
+        'form': form,
+    }
+    return render(request, 'instaclone/home.html', params)
+
+
+@login_required(login_url='login')
+def profile(request, username):
+    """
+    route to own user profile
+    """
+    images = request.user.userprofile.userposts.all()
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateUserProfileForm(instance=request.user.userprofile)
+    params = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'images': images,
+
+    }
+    return render(request, 'instaclone/profile.html', params)
+
+
+@login_required(login_url='login')
+def user_profile(request, username):
+    """
+    route to return another users profile
+    """
+    user_profile = get_object_or_404(User, username=username)
+    if request.user == user_profile:
+        return redirect('profile', username=request.user.username)
+    user_posts = user_profile.userprofile.userposts.all()
+    followers = Follow.objects.filter(followed=user_profile.userprofile)
+    follow_status = None
+    for follower in followers:
+        follow_status = request.user.username == str(follower.follower.user)
+
+    params = {
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'followers': followers,
+        'follow_status': follow_status
+    }
+    return render(request, 'instaclone/user_profile.html', params)
+
+
+@login_required(login_url='login')
+def post_comment(request, id):
+    """
+    route to create new comments
+    """
+    image = get_object_or_404(UserPost, pk=id)
+    is_liked = bool(image.likes.filter(id=request.user.id).exists())
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            save_comment = form.save(commit=False)
+            save_comment.post = image
+            save_comment.user = request.user.userprofile
+            save_comment.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = CommentForm()
+    params = {
+        'image': image,
+        'form': form,
+        'is_liked': is_liked,
+        'total_likes': image.total_likes()
+    }
+    return render(request, 'instaclone/single_post.html', params)
+
+
+class LikePost(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        id = self.kwargs.get('id')
+        object = get_object_or_404(UserPost, pk=id)
+        url = object.get_absolute_url()
+        user = self.request.user
+        if user in object.likes.all():
+            object.likes.remove(user)
+        else:
+            object.likes.add(user)
+        return url
+
+
+def like_post(request):
+    # image = get_object_or_404(Post, id=request.POST.get('image_id'))
+    image = get_object_or_404(UserPost, id=request.POST.get('id'))
+    is_liked = False
+    if image.likes.filter(id=request.user.id).exists():
+        image.likes.remove(request.user)
+    else:
+        image.likes.add(request.user)
+    is_liked = False
+    params = {
+        'image': image,
+        'is_liked': is_liked,
+        'total_likes': image.total_likes()
+    }
+    if request.is_ajax():
+        html = render_to_string('instaclone/like_page.html', params, request=request)
+        return JsonResponse({'form': html})
+
+
+def follow(request, to_follow):
+    if request.method == 'GET':
+        user_profile = UserProfile.objects.get(pk=to_follow)
+        follow = Follow(follower=request.user.userprofile, followed=user_profile)
+        follow.save()
+        return redirect('user_profile', user_profile.user.username)
+
+
+def unfollow(request, to_unfollow):
+    if request.method == 'GET':
+        user_profile = UserProfile.objects.get(pk=to_unfollow)
+        unfollow = Follow.objects.filter(follower=request.user.userprofile, followed=user_profile)
+        unfollow.delete()
+        return redirect('user_profile', user_profile.user.username)
+
+
+@login_required(login_url='login')
+def search_profile(request):
+    if request.GET['search_user'] and 'search_user' in request.GET:
+        name = request.GET.get("search_user")
+        results = UserProfile.search_profile(name)
+        for r in results:
+            print(r.user)
+        message = f'{name}'
+        params = {
+            'results': results,
+            'message': message,
+            'name': name,
+
+        }
+        return render(request, 'instaclone/results.html', params)
+    else:
+        message = "You haven't searched for any user"
+    return render(request, 'instaclone/results.html', {'message': message})
