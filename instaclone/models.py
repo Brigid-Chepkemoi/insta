d20c502532af5a48adec7f1795f0f61850a0dd95
brie <briememo95@gmail.com>
2022-06-06 20:47:51 +0300

    add models to add, delete and view pictures

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,112 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """
+    model for the user including related methods
+    """
+    user = models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
+    username = models.TextField(max_length=150)
+    bio = models.CharField(max_length=400, blank=True, null=True, default="simple bio")
+    # profile_picture = models.ImageField(upload_to='images/profile/', default='images/profile/user.png')
+    profile_picture = CloudinaryField('image', default="https://res.cloudinary.com/dd5ab8mp3/image/upload/v1634659738"
+                                                       "/images/profile/user.jpg")
+    location = models.CharField(max_length=70, blank=True, null=True)
+
+    @staticmethod
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        """
+        receives signal once user model is saved, if user was created we create a UserProfile instance
+        """
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @staticmethod
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
+
+    def save_profile(self):
+        # self.save()
+        pass
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return f'{self.user.username}: profile'
+
+    @classmethod
+    def search_profile(cls, search_term):
+        """
+        method to search for users
+        """
+        return cls.objects.filter(user__username__icontains=search_term).all()
+
+
+class UserPost(models.Model):
+    """
+    model for post by User
+    """
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='userposts')
+    # image = models.ImageField(upload_to='images/posts/')
+    image = CloudinaryField('image')
+    caption = models.CharField(max_length=250, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    name = models.CharField(max_length=250, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def get_absolute_url(self):
+        return f"/post/{self.id}"
+
+    @property
+    def get_all_comments(self):
+        return self.comments.all()
+
+    def save_image(self):
+        # self.save()
+        pass
+
+    def delete_image(self):
+        self.delete()
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return f'{self.user.username} Post'
+
+
+class UserComment(models.Model):
+    """
+    model for the user comments in relation to User and Posts
+    """
+    comment = models.TextField()
+    post = models.ForeignKey(UserPost, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return f'{self.user.username} Post'
+
+
+class Follow(models.Model):
+    """
+    model for User followers and User following
+    """
+    follower = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return f'{self.follower} Follow'
--- instaclone/tests.py
+++ instaclone/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from .models import UserProfile, UserPost
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='zoo-test')
+        self.user.save()
+
+        self.profile_test = UserProfile(id=13, username='image', profile_picture='default.jpg',
+                                        bio='this is a test profile',
+                                        user=self.user)
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, UserProfile))
+
+    def test_save_profile(self):
+        self.profile_test.save_profile()
+        after = UserProfile.objects.all()
+        self.assertTrue(len(after) > 0)
+
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.user = User(username='zoo-codes')
+        self.user.save()
+        self.profile_test = UserProfile(id=3, username='zoo-test', user=self.user)
+        # self.profile_test.save()
+
+        self.image_test = UserPost(image='user.png', caption='default test', user=self.profile_test)
+
+    def tearDown(self):
+        UserPost.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, UserPost))
+
+    def test_save_image(self):
+        self.image_test.save()
+        images = UserPost.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image_test.save()
+        self.image_test.delete()
+        after = UserPost.objects.all()
+
